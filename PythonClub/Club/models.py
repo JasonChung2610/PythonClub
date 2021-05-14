e9f0d0d041a4a1de994259cb4b23ecfd8e0d1491
Jason <nhimchung2610@gmail.com>
2021-05-13 23:39:28 -0700

    Model commit

--- PythonClub/Club/models.py
+++ PythonClub/Club/models.py
@@ -2,39 +2,59 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class ClubType(models.Model):
-    typename=models.CharField(max_length=255)
-    typedescription=models.TextField(null=True, blank=True)
-
+class Meeting(models.Model):
+    meeting_tittle=models.CharField(max_length=255)
+    meeting_date=models.DateField(null=True)
+    meeting_time=models.TimeField(null=True)
+    location=models.TextField(null=True, blank=True)
+    Agenda=models.TextField(null=True, blank=True)
     def __str__(self):
-        return self.typename
+        return self.meeting_tittle  
     
     class Meta: 
-        db_table='clubtype'
-class Product(models.Model):
-    productname=models.CharField(max_length=255)
-    productname=models.ForeignKey(ClubType, on_delete=models.DO_NOTHING)
-    user=models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    dateentered=models.DateField()
-    price=models.DecimalField(max_digits=6, decimal_places=2)       
-    productur1=models.URLField()
-    description=models.TextField()
+        db_table='meeting'
+class MeetingMinute(models.Model):
+    meeting_id=models.CharField(max_length=255)
+    meeting_id=models.ForeignKey(Meeting, on_delete=models.DO_NOTHING)
+    attendance=models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    #dateentered=models.DateField()
+    #price=models.DecimalField(max_digits=6, decimal_places=2)       
+    #productur1=models.URLField()
+    #description=models.TextField()
+    minutestext=models.TextField(null=True, blank=True)
+    def __str__(self):
+        return self.meeting_id
+
+    class Meta:
+        db_table='meetingminute'
+
+class Resource(models.Model):
+    resource_name=models.CharField(max_length=255, null= True)
+    resource_type=models.CharField(max_length=255, null= True)
+    URL=models.URLField(null=True)
+    date_entered=models.DateField(null=True)
+    user_id=models.ForeignKey(User, on_delete=models.CASCADE)
+    description=models.TextField(null=True, blank=True)
+    #product=models.ForeignKey(MeetingMinute, on_delete=models.CASCADE)
     
-    def __str__(self):
-        return self.productname
-
-    class Meta:
-        db_table='product'
-
-class Review(models.Model):
-    title=models.CharField(max_length=255)
-    user=models.ForeignKey(User, on_delete=models.CASCADE)
-    product=models.ForeignKey(Product, on_delete=models.CASCADE)
-    reviewdate=models.DateField()
-    reviewtext=models.TextField()
 
     def __str__(self):
-        return self.title
+        return self.resource_name
 
     class Meta:
-        db_table='review'
+        db_table='resource'
+
+class Event(models.Model):
+    event_title=models.CharField(max_length=255)
+    location=models.TextField(null=True, blank=True)
+    date=models.DateField(null=True)
+    time=models.TimeField(null=True)
+    description=models.TextField(null=True, blank=True)
+    user_id=models.CharField(max_length=255, null = True)
+
+    def __str__(self):
+        return self.event_title
+    
+    class Meta: 
+        db_table='event'
+    
